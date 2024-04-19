a9608dc26a7c6c5b9a84a4f8523f757c505b0ca5
satrijo <mail@satrio.dev>
2024-04-20 01:25:31 +0700

    simple upload using Hono Js and Bun

---
+++ components/index.jsx
@@ -0,0 +1,88 @@
+import { css, Style } from "hono/css";
+const Upload = ({ dataList }) => {
+  const htmlClass = css`
+    margin: auto;
+    margin-top: 100px;
+    text-align: center;
+    width: 50%;
+    border: 1px solid gray;
+    border-radius: 10px;
+    padding: 100px;
+    padding-top: 100px;
+    display: block;
+  `;
+
+  const imageList = css`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin-top: 100px;
+
+    img {
+      width: 250px;
+      height: 150px;
+      object-fit: cover;
+      margin: 10px;
+      border-radius: 10px;
+      border: 1px solid black;
+    }
+    ul {
+      list-style: none;
+      padding: 0;
+      margin: 0;
+    }
+
+    p {
+      color: black;
+    }
+  `;
+
+  const dateTime = (unix) => {
+    if (!unix) return;
+
+    unix = parseInt(unix);
+    const date = new Date(unix);
+    return date.toLocaleString();
+  };
+
+  // sort dataList
+  dataList.sort((a, b) => b.localeCompare(a));
+
+  return (
+    <html>
+      <head>
+        <Style />
+      </head>
+      <body class={htmlClass}>
+        <form method="POST" action="/upload" enctype="multipart/form-data">
+          <input type="file" name="image" accept="image/*" />
+          <button type="submit">Upload</button>
+        </form>
+
+        <div class={imageList}>
+          {dataList.map(
+            (item, index) =>
+              index < 10 && (
+                <>
+                  <a
+                    href={item}
+                    key={index}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={item} key={index} />
+                    <p>
+                      <b>File di Upload </b>{" "}
+                      {dateTime(item.split("/").pop().split(".")[0])}
+                    </p>
+                  </a>
+                </>
+              )
+          )}
+        </div>
+      </body>
+    </html>
+  );
+};
+
+export default Upload;
---
+++ index.js
@@ -0,0 +1,61 @@
+import Index from './components'
+import { Hono } from 'hono'
+import { serveStatic } from 'hono/bun'
+import { basicAuth } from 'hono/basic-auth'
+import { Glob } from "bun";
+
+const glob = new Glob("**/*.{jpg,jpeg,png,gif,webp,svg}", { cwd: "src/public/images/" });
+
+const app = new Hono()
+
+app.use('*', basicAuth({ username: 'aljabar', password: 'ci1994@' }))
+
+app.use('/public/*', serveStatic({ root: './' }))
+
+app.get('/', async (c) => {
+  let dataList = []
+  for await (const file of glob.scan(".")) {
+    dataList.push(file)
+  }
+  return c.html(<Index dataList={dataList} />)
+})
+
+app.get('/upload', (c) => {
+  return c.redirect('/')
+})
+
+app.post('/upload', async (c) => {
+  const body = await c.req.parseBody()
+  const arrayBuffer = await body.image.arrayBuffer()
+
+  const validFileTypes = new Set([
+    'image/jpeg',
+    'image/png',
+    'image/gif',
+    'image/webp',
+    'image/svg+xml'
+  ])
+
+  const validateFileTypeImageOnly = (file) => validFileTypes.has(file.type)
+  if (body.image) {
+    if (validateFileTypeImageOnly(body.image)) {
+      const randomId = Date.now()
+      let data = Buffer.from(arrayBuffer)
+      const path = 'public/images/'
+      const save = path + randomId + '.jpeg'
+      // const save = path + randomId + '.' + body.image.type.split('/')[1]
+      Bun.write(save, data)
+      return c.redirect('/')
+    } else {
+      return c.json({ status: 'error', message: 'Invalid file type. Only images are allowed.' })
+    }
+  } else {
+    return c.json({ status: 'error', message: 'No file uploaded.' })
+  }
+})
+
+
+export default {
+  fetch: app.fetch,
+  port: 1994,
+}
